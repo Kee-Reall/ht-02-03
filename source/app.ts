8342dca8b2a9a0aa31b061ed0c17493264a606ad
Kee-Reall <kirill_bezrodny@mail.ru>
2022-11-22 00:13:06 +0500

    put scripts

--- source/app.ts
+++ source/app.ts
@@ -1,15 +1,19 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/',(req,res) =>{
-    res.json({'test':'jest'})
+    res.status(400).json({'test':'jest'})
 })
 
 app.post('/',(req,res) => {
     res.json(...req.body)
+})
+
+app.patch('/',(req:Request, res: Response) => {
+    res.json({'patch':true})
 })
 
 export default app
--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
 import app from "./app";
 const port = process.env.port ?? 3000
 
-app.listen(port,()=> console.log('Server is running on port ' + port))
+app.listen(port,()=> console.log('Server is running on port witch was changed ' + port))
