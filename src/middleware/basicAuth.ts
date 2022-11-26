d466b7c498a42494b3d3c1bda0acc589734cb6e7
Kee-Reall <kirill_bezrodny@mail.ru>
2022-11-26 19:23:35 +0500

    fixed auth middleware #2

--- src/middleware/basicAuth.ts
+++ src/middleware/basicAuth.ts
@@ -5,9 +5,14 @@ export const basicAuth = (req: Request, res: Response, next: NextFunction) => {
     if(req.method === httpMethod.get) {
         return next()
     }
-    const [type, auth64] = req.headers.authorization!.split(' ')
+    const auth = req.headers.authorization
+    if(!Array.isArray(auth)){
+        res.sendStatus(httpStatus.notAuthorized)
+        return
+    }
+    const [type,auth64] = auth
     const [login,password] = Buffer.from(auth64 ?? '', 'base64').toString('ascii').split(':')
-    if(login === 'admin' && password === 'qwerty' && type === "Basic") {
+    if(login === 'admin' && password === 'qwerty' && type === 'Basic') {
         return next()
     }
     res.sendStatus(httpStatus.notAuthorized)
