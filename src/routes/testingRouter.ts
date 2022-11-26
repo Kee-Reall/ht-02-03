09ec7e6f4511811e5c3aeb0adf0d3f55c03a6958
Kee-Reall <kirill_bezrodny@mail.ru>
2022-11-26 18:34:46 +0500

    clear bug fixed

--- src/routes/testingRouter.ts
+++ src/routes/testingRouter.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
 import { testingController} from "../controllers/testingController";
-
 export const testingRouter = Router()
-const root = '/'
-const param = root + ':id'
 
-testingRouter.delete(root,  testingController.clearAll)
-testingRouter.delete(param, testingController.deprecated)
+testingRouter.delete('all-data',  testingController.clearAll)
 testingRouter.all('*', testingController.deprecated)
