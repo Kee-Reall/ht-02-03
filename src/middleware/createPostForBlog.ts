1552c683b6d45ce6997f32d6a5bfbfa2c42a50fa
Kee-Reall <admin.bezrodnyi@wms_net.local>
2022-12-08 11:50:19 +0500

    refactored createPost middlewares

--- src/middleware/createPostForBlog.ts
+++ src/middleware/createPostForBlog.ts
@@ -1,34 +1,14 @@
-import { body, param } from "express-validator"
+import { param } from "express-validator"
 import { errorHas } from "./errorHas"
 import {message} from "../enums/messageEnum";
 import {checkForExistingBlog} from "../helpers/checkForExistingBlog";
+import { postInputValidator } from "../helpers/postInputValidator";
 
 export const createPostForBlogMiddleware = [
-    body('title').exists()
-        .withMessage(message.requireField)
-        .isString()
-        .withMessage(message.invalidType)
-        .trim()
-        .isLength({min:1,max:30})
-        .withMessage(message.length),
-
-    body('shortDescription').exists()
-        .withMessage(message.requireField)
-        .isString()
-        .withMessage(message.invalidType)
-        .trim()
-        .isLength({min:1,max:100})
-        .withMessage(message.length),
-
-    body('content').exists()
-        .withMessage(message.requireField)
-        .isString()
-        .withMessage(message.invalidType)
-        .trim()
-        .isLength({min:1,max:1000})
-        .withMessage(message.length),
+    ...postInputValidator,
 
     param('id').custom(checkForExistingBlog)
         .withMessage(message.notExist),
+
     errorHas
 ]
--- src/middleware/potstMiddleware.ts
+++ src/middleware/potstMiddleware.ts
@@ -2,36 +2,16 @@ import { body } from "express-validator"
 import { errorHas } from "./errorHas"
 import {message} from "../enums/messageEnum";
 import {checkForExistingBlog} from "../helpers/checkForExistingBlog";
+import { postInputValidator } from "../helpers/postInputValidator";
 
 export const postMiddlewares = [
-    body('title').exists()
-        .withMessage(message.requireField)
-        .isString()
-        .withMessage(message.invalidType)
-        .trim()
-        .isLength({min:1,max:30})
-        .withMessage(message.length),
-
-    body('shortDescription').exists()
-        .withMessage(message.requireField)
-        .isString()
-        .withMessage(message.invalidType)
-        .trim()
-        .isLength({min:1,max:100})
-        .withMessage(message.length),
-
-    body('content').exists()
-        .withMessage(message.requireField)
-        .isString()
-        .withMessage(message.invalidType)
-        .trim()
-        .isLength({min:1,max:1000})
-        .withMessage(message.length),
+    ...postInputValidator,
 
     body('blogId').exists()
         .withMessage(message.requireField)
         .trim()
         .custom(checkForExistingBlog)
         .withMessage(message.notExist),
+        
     errorHas
 ]
