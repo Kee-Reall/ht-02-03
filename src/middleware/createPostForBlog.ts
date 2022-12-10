f03dc0c517216a66195d7e153bd6647209457692
Kee-Reall <kirill_bezrodny@mail.ru>
2022-12-10 18:15:06 +0500

    fixed a 404 error while create post from blog

--- src/middleware/createPostForBlog.ts
+++ src/middleware/createPostForBlog.ts
@@ -3,12 +3,18 @@ import { errorHas } from "./errorHas"
 import {message} from "../enums/messageEnum";
 import {checkForExistingBlog} from "../helpers/checkForExistingBlog";
 import { postInputValidator } from "../helpers/postInputValidator";
+import {idErrorHas} from "../helpers/idErrorHas";
 
 export const createPostForBlogMiddleware = [
     ...postInputValidator,
 
+
+
+    errorHas,
+
     param('id').custom(checkForExistingBlog)
         .withMessage(message.notExist),
 
-    errorHas
+    idErrorHas
+
 ]
