51ddc92410776988570fac859de4d7aae6e710b3
Kee-Reall <kirill_bezrodny@mail.ru>
2022-12-11 15:02:30 +0500

    chage get from posts error logic

--- src/middleware/createPostForBlog.ts
+++ src/middleware/createPostForBlog.ts
@@ -1,20 +1,15 @@
-import { param } from "express-validator"
-import { errorHas } from "./errorHas"
+import {param} from "express-validator"
+import {errorHas} from "./errorHas"
 import {message} from "../enums/messageEnum";
 import {checkForExistingBlog} from "../helpers/checkForExistingBlog";
-import { postInputValidator } from "../helpers/postInputValidator";
+import {postInputValidator} from "../helpers/postInputValidator";
 import {idErrorHas} from "../helpers/idErrorHas";
 
 export const createPostForBlogMiddleware = [
     ...postInputValidator,
-
-
-
     errorHas,
-
     param('id').custom(checkForExistingBlog)
         .withMessage(message.notExist),
 
     idErrorHas
-
 ]
--- src/middleware/postsFromBlogs.ts
+++ src/middleware/postsFromBlogs.ts
@@ -1,15 +1,8 @@
-import {query, param} from "express-validator";
-import {message} from "../enums/messageEnum";
-import {errorHas} from "./errorHas";
+import {param} from "express-validator";
 import {checkForExistingBlog} from "../helpers/checkForExistingBlog";
+import {idErrorHas} from "../helpers/idErrorHas";
 
 export const getPostsByBlogMiddleware = [
-    query('sortBy').exists()
-        .withMessage(message.requireField)
-        .trim()
-        .isLength({min:1,max:500})
-        .withMessage(message.length),
-
     param('id').custom(checkForExistingBlog),
-    errorHas
+    idErrorHas
 ]
