e961ee34badd1df871961f4b7ba3f9bc7ce53208
Kee-Reall <kirill_bezrodny@mail.ru>
2023-01-30 19:03:40 +0500

    enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import {apiRouter} from "./routes/apiRoutes";
 import cookiesParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
+app.use(cors({}))
 app.use(express.json())
 app.use(cookiesParser())
 app.set('trust-proxy', true)
