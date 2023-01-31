e7191f9d4d39c0c33c27cae824d836afa493b452
Kee-Reall <kirill_bezrodny@mail.ru>
2023-01-31 21:53:40 +0500

    refactored device getting

--- src/services/security-service.ts
+++ src/services/security-service.ts
@@ -1,15 +1,11 @@
 import {securityViewModel} from "../models/SecurityModel";
-import {refreshTokenPayload} from "../models/refreshTokensMeta";
 import {jwtService} from "./jwt-service";
 import {queryRepository} from "../repositories/queryRepository";
 
 class SecurityService {
     public async getAllSessionsByToken(token: string): Promise< Array<securityViewModel> | null> {
-        const payload: refreshTokenPayload = await jwtService.verifyRefreshToken(token) as refreshTokenPayload
-        if(!payload) {
-            return null
-        }
-        return await queryRepository.getTokensForUser(payload.userId)
+        const payload = await jwtService.verifyRefreshToken(token)
+        return payload ? await queryRepository.getTokensForUser(payload.userId) : null
     }
 }
 
