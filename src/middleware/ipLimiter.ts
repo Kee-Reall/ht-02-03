43d3ea7d7a513ed29dbff2f12251454a311e3eee
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-04 21:53:52 +0500

    ht09 refactored limiter middleware

--- src/middleware/ipLimiter.ts
+++ src/middleware/ipLimiter.ts
@@ -3,15 +3,15 @@ import {attemptsRepository} from "../repositories/attemptsRepository";
 import {subSeconds} from "date-fns"
 import {httpStatus} from "../enums/httpEnum";
 
-export const ipLimiter: RequestHandler = async (req,res,next) => {
-    const {ip, url} = req
-    const endpointAndIp = ip + url
+export const ipLimiter: RequestHandler = async (req, res, next) => {
+    const {ip, headers, url, socket: {remoteAddress}} = req
+    const endpointAndIp = (headers['x-forwarded-for'] || remoteAddress || ip) + url
     const date = new Date(Date.now())
-    await attemptsRepository.addNewAttempt({endpointAndIp,date: date.toISOString()})
+    await attemptsRepository.addNewAttempt({endpointAndIp, date: date.toISOString()})
     const limit: number = 5
-    const tenSecAgo = subSeconds(date,10).toISOString()
-    const shouldBlock = await attemptsRepository.getAttemptsCount(endpointAndIp,tenSecAgo) > limit
-    if(shouldBlock){
+    const tenSecAgo = subSeconds(date, 10).toISOString()
+    const shouldBlock = await attemptsRepository.getAttemptsCount(endpointAndIp, tenSecAgo) > limit
+    if (shouldBlock) {
         return res.sendStatus(httpStatus.tooManyRequests)
     }
     next()
