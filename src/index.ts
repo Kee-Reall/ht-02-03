f586d20ecacbca229492ae3e2f4bb2448ff46bbc
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-06 21:52:42 +0500

    ht10 looks finished #2

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import app from "./app";
-import { runDb } from "./adapters/mongoConnectorCreater";
 import {mRunDb} from "./adapters/mongooseCreater";
 
 const port = process.env.PORT ?? 8000;
 
 async function start() {
-    const isDbAvailable = await runDb()
     const isMongooseReady = await mRunDb()
     const hasJwtSecret = process.env.JWT_SECRET && true
-    if(isDbAvailable && hasJwtSecret && isMongooseReady) {
+    if(hasJwtSecret && isMongooseReady) {
         app.listen(port,() => console.log('Server is running on port\n' + port))
     } else {
         throw new Error("failed to connect database")
