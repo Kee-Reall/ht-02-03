2fb54b531a86b0ab525678f3509511247f4e119a
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-06 22:54:24 +0500

    Revert "ht10 looks finished #2"
    This reverts commit f586d20ecacbca229492ae3e2f4bb2448ff46bbc.

--- src/adapters/mongoConnectorCreater.ts
+++ src/adapters/mongoConnectorCreater.ts
@@ -1,36 +1,36 @@
-// import { MongoClient } from "mongodb"
-// import {postViewModel} from "../models/postsModel";
-// import {blogViewModel} from "../models/blogModel";
-// import {userDbModel} from "../models/userModel";
-// import {commentsDbModel} from "../models/commentsModel";
-// import * as dotenv from 'dotenv'
-// import {refreshTokensMeta} from "../models/refreshTokensMeta";
-// import {attemptsModel} from "../models/attemptsModel";  // idk why but it repairs supertest inside tests
-// dotenv.config()
-//
-// const dbUri: string = process.env.MONGO_URI as string
-// if(!dbUri) {
-//     throw new Error('you should set a MONGO_URI in .env')
-// }
-//
-// export const client = new MongoClient(dbUri)
-// export const db = client.db('ht-03')
-// export const posts = db.collection<postViewModel>('posts')
-// export const blogs = db.collection<blogViewModel>('blogs')
-// export const users = db.collection<userDbModel>('users')
-// export const comments = db.collection<commentsDbModel>('comments')
-//
-// export const sessions = db.collection<refreshTokensMeta>('sessions')
-// export const attempts = db.collection<attemptsModel>('attempts')
-// export async function runDb(): Promise<boolean> {
-//     try {
-//         await client.connect()
-//         await client.db('test').command({ping: 1})
-//         console.log('db Connection success\n Creating required collections')
-//         return true
-//     } catch (error) {
-//         console.log('Can not connect to db \n' + error)
-//         await client.close()
-//         return false
-//     }
-// }
+import { MongoClient } from "mongodb"
+import {postViewModel} from "../models/postsModel";
+import {blogViewModel} from "../models/blogModel";
+import {userDbModel} from "../models/userModel";
+import {commentsDbModel} from "../models/commentsModel";
+import * as dotenv from 'dotenv'
+import {refreshTokensMeta} from "../models/refreshTokensMeta";
+import {attemptsModel} from "../models/attemptsModel";  // idk why but it repairs supertest inside tests
+dotenv.config()
+
+const dbUri: string = process.env.MONGO_URI as string
+if(!dbUri) {
+    throw new Error('you should set a MONGO_URI in .env')
+}
+
+export const client = new MongoClient(dbUri)
+export const db = client.db('ht-03')
+export const posts = db.collection<postViewModel>('posts')
+export const blogs = db.collection<blogViewModel>('blogs')
+export const users = db.collection<userDbModel>('users')
+export const comments = db.collection<commentsDbModel>('comments')
+
+export const sessions = db.collection<refreshTokensMeta>('sessions')
+export const attempts = db.collection<attemptsModel>('attempts')
+export async function runDb(): Promise<boolean> {
+    try {
+        await client.connect()
+        await client.db('test').command({ping: 1})
+        console.log('db Connection success\n Creating required collections')
+        return true
+    } catch (error) {
+        console.log('Can not connect to db \n' + error)
+        await client.close()
+        return false
+    }
+}
