c5c414e4f05406f807adc09aeef077b1300146b4
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-08 20:02:25 +0500

    i've set onlu local host2

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app = express()
 app.use(cors({
-    origin: "http//localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true
 }))
 app.use(express.json())
