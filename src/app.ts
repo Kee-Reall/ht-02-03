dfbba8785fa35c668f0a19ed51c80cf2046ede9c
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-08 19:41:32 +0500

    seted a cors origin

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app = express()
 app.use(cors({
-    origin: "*"
+    origin: ["http://localhost:3000"]
 }))
 app.use(express.json())
 app.use(cookiesParser())
---
+++ src/middleware/setHeaderCORS.ts
@@ -0,0 +1,6 @@
+import {RequestHandler} from "express";
+
+const setHeaderCORS: RequestHandler = (req, res, next) => { //not use for now
+    const {ip} = req
+    next()
+}
