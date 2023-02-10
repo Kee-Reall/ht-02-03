cf450afc9eef6701215b251322c031c603f245f3
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-10 19:44:30 +0500

    cords#6

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express from 'express'
 import {apiRouter} from "./routes/apiRoutes";
 import cookiesParser from 'cookie-parser'
-import cors from 'cors'
+//import cors from 'cors'
 
 const app = express()
-app.use(cors({
+/*app.use(cors({
     origin: "*",
     credentials: true,
     optionsSuccessStatus: 200
-})) 
+})) */
+
+app.use(function(req, res, next) {
+    //@ts-ignore
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json())
 app.use(cookiesParser())
 app.set('trust-proxy', true)
