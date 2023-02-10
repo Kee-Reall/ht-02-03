6b30f8e5c1c63563c01accaf83fddbccfd0269d8
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-10 19:57:24 +0500

    cords#6

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,  Authorization");
     next();
 });
 app.use(express.json())
