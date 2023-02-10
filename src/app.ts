847839512f791ad650e04c0623b78ab96d1134cf
Kee-Reall <kirill_bezrodny@mail.ru>
2023-02-10 20:28:14 +0500

    fck crs get back

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,14 @@
 import express from 'express'
 import {apiRouter} from "./routes/apiRoutes";
 import cookiesParser from 'cookie-parser'
-//import cors from 'cors'
+import cors from 'cors'
 
 const app = express()
-/*app.use(cors({
-    origin: "*",
+app.use(cors({
+    origin: ['http://localhost:3000'],
     credentials: true,
-    optionsSuccessStatus: 200
-})) */
+}))
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,  Authorization");
-    next();
-});
 app.use(express.json())
 app.use(cookiesParser())
 app.set('trust-proxy', true)
